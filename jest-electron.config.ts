0b27f545100c33f4705e42d47847229f9a672349
Khoi Pham <pckhoi@gmail.com>
2021-11-14 09:27:44 +0700

    feat: upgrade to jest v27

--- jest-electron.config.ts
+++
@@ -1,9 +0,0 @@
-import config from "./jest.config";
-
-export default {
-  ...config,
-  runner: "jest-electron/runner",
-  testEnvironmentOptions: {
-    testEnvironment: "jest-electron/environment",
-  },
-};
